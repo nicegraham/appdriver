bc16ae1df8de1debb0343ae5aedc917d2ac5b44d
tokunbo <tokunbo.george@rackspace.com>
2013-01-04 16:20:45 -0800

    Create HealthStatusServlet.java
    Adding HealthStatusServlet.java

---
+++ android/src/com/google/android/testing/nativedriver/server/HealthStatusServlet.java
@@ -0,0 +1,52 @@
+package com.google.android.testing.nativedriver.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import android.util.Log;
+
+public class HealthStatusServlet extends HttpServlet
+{ 
+ private ServletConfig config;
+ @Override
+ public void init(ServletConfig config) throws ServletException
+ {
+  this.config = config;
+ }
+
+ @Override 
+ public void service(ServletRequest req, ServletResponse res)
+ {
+     try
+     {
+  	res.getWriter().
+		println("{'status': 0, 'sessionId': null, 'driverName':'Appdriver'}");
+     }
+     catch(Exception e)
+     {
+                Log.e("appdriver","HealthStatusServlet encountered a problem with "+httpreq.getPathInfo()+" - "+e.toString());
+     }
+ }
+
+ @Override
+ public String getServletInfo()
+ {
+     return "For replying Grid2's /wd/hub/status queries.";
+ }
+
+ @Override
+ public ServletConfig getServletConfig()
+ {    
+    return this.config;
+ }
+
+ @Override
+ public void destroy()
+ {
+  //Do Nothing
+ }
+}
